9d8a49b18a2a8dd158618f4cafb81154d9a23f82
lee000403 <dong000403@gmail.com>
2023-06-26 18:17:54 +0900

    first commit

---
+++ src/main/java/com/example/toy_servlet/ToyServletApplication.java
@@ -0,0 +1,13 @@
+package com.example.toy_servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyServletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyServletApplication.class, args);
+	}
+
+}
