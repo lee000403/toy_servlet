2707f8cd1ac8891d2e1bff24c18f2460bdaaed6c
Your Name <you@example.com>
2023-06-27 19:28:28 +0900

    김민정 push

---
+++ src/main/java/com/example/toy_servlet/daos/PollsInformationDao.java
@@ -0,0 +1,30 @@
+package com.example.toy_servlet.daos;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.HashMap;
+
+import com.example.toy_servlet.commons.Commons;
+
+public class PollsInformationDao {
+    public HashMap<String, String> selectAll() {
+        HashMap<String, String> hashMap = new HashMap<>();
+        try {
+            Commons commons = new Commons();
+            Statement statement = commons.getStatement();
+
+            String query = "SELECT * FROM db_poll.respondents;";
+            ResultSet resultSet = statement.executeQuery(query);
+
+            if (resultSet.next()) {
+                hashMap.put("RESPONDENTS", resultSet.getString("RESPONDENTS"));
+                hashMap.put("RESPONDENTS_ID", resultSet.getString("RESPONDENTS_ID"));
+                hashMap.put("PASSWORD", resultSet.getString("PASSWORD"));
+                hashMap.put("PHONENUMBER", resultSet.getString("PHONENUMBER"));
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return hashMap;
+    }
+}
